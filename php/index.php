4592e79c76d487d119335aaf5888046e98a31693
DucManh2710 <tdmanh2710@gmail.com>
2023-11-08 15:03:41 +0700

    SLIDE 13 PHP

--- php/index.php
+++ php/index.php
@@ -1,3 +1,8 @@
 <?php
-    echo 'Xin chao Tran Duc Manh';
+    echo 'My first PHP file';
+    echo '<h3> Hello World!</h3>';
+    echo '<select>';
+    echo "<option>1</option>";
+    echo "<option>2</option>";
+    echo "</select>";
 ?>
