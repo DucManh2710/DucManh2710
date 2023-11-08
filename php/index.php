f777c797c0787ba56995e0eafc4577a84afb1d3f
DucManh2710 <tdmanh2710@gmail.com>
2023-11-08 15:20:07 +0700

    SLIDE 34 PHP

--- php/index.php
+++ php/index.php
@@ -1,11 +1,18 @@
-<?php
-$Subject = "Lap trinh ung dung web ";
-$UniversityName = "HUMG";
-$firtsNumber = 2;
-$secondtNumber = 2;
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <form action="5-formGet.php" method="get">       
+        <p>Name: </p>
+        <input type="text" name="name"><br>
+        <p>Email: </p>
+        <input type="text" name="email"><br>
+        <p></p>
+        <input type="submit">
+    </form>
+</body>
+</html>
 
-echo " Hoc ". $Subject . "o phong hoc 502 tai truong " . $UniversityName . "<br>";
-echo " Hoc $Subject o phong 502 tai truong $UniversityName <br> ";
-echo " <h1> $UniversityName </h1>";
-echo $firtsNumber + $secondtNumber;
-?>
