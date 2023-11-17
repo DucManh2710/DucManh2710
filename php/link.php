20ec31327554baff1638200586a3e8044af516c2
DucManh2710 <tdmanh2710@gmail.com>
2023-11-17 20:00:06 +0700

    SLIDE 11

--- php/link.php
+++ php/link.php
@@ -3,15 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>t</title>
 </head>
 <body>
    <?php
-        $a = $_GET["a"];
-        $b = $_GET["b"];
-        $c = $_GET["c"];
-        $delta = ($b*$b)-4*$a*$c;
-        if($delta < 0 )
-        echo 
+   echo "THE GAME";
+   if ($_GET["response"] == "yes")
+   {
+    echo " HAS BEEN QUITED";
+   }
+   if ($_GET["response"] == "no")
+   {
+    echo "WILL BE CONTINUE IN 3 SECONDS";
+   }
+   echo " <br> AFTER IF STATEMENT"
+    ?>
 </body>
 </html>
