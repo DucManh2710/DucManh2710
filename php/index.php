60041d1bdb67abba9010fc13e573499fd16a0c5b
DucManh2710 <tdmanh2710@gmail.com>
2023-11-22 15:21:28 +0700

    SLIDE 33

--- php/index.php
+++ php/index.php
@@ -8,14 +8,9 @@
 <body>
     <?php
         $exchangeRate = array("USD" >=22300, "EUR">=27300, "SGD">=17000, "JPY">=120);
-        $exchangeRate[] = 600;
-        $exchangeRate[] = 16000;
-        $exchangeRate[0] = 22200;   
-        echo "<h3>Exchange rate  to VND</h3>";
-        echo "USD:" . $exchangeRate['USD'] . "<br>"; 
-        echo "EUR:" . $exchangeRate['EUR'] . "<br>"; 
-        echo "SGD:" . $exchangeRate['SGD'] . "<br>"; 
-        echo "JPY:" . $exchangeRate['JPY'] . "<br>"; 
+        $amount = $_GET['amount'];
+        $currency = $_GET["currency"];
+        echo " $amount USD is equal " . number_format($amount * $exchangeRate[$currency]) . " VND";
     ?>
 </body>
 </html>
