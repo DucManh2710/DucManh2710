c6f3699dbd6e3234b7cfcec9d17a370cb37bdd6f
DucManh2710 <tdmanh2710@gmail.com>
2023-11-22 15:11:58 +0700

    SLIDE 28

--- php/index.php
+++ php/index.php
@@ -7,23 +7,17 @@
 </head>
 <body>
     <?php
-        $currency = $_GET['currency'];
-        echo " $amount $currency is equal ";
-        switch ($currency)
-        {
-            case "USD":
-                echo $amount * $USD;
-                break;
-            case "EUR":
-                echo $amount * $EUR;
-                break;
-            case "SGD":
-                echo $amount * $SGD;
-                break;
-            case "JPY":
-                echo $amount * $JPY;
-                break;
-        }
+        $exchangeRate = array(22300, 27300, 17000, 120);
+        $exchangeRate[] = 600;
+        $exchangeRate[] = 16000;
+        $exchangeRate[0] = 22200;
+        echo "<h3>Exchange rate  to VND</h3>";
+        echo "USD: $exchangeRate[0]<br>"; 
+        echo "EUR: $exchangeRate[1]<br>"; 
+        echo "SGD: $exchangeRate[2]<br>"; 
+        echo "JPY: $exchangeRate[3]<br>"; 
+        echo "CHN: $exchangeRate[4]<br>";   
+        echo "AUD: $exchangeRate[5]<br>"; 
     ?>
 </body>
 </html>
