675d29a487f706f93a20feaf594e2306a5a3a9c3
DucManh2710 <tdmanh2710@gmail.com>
2023-12-05 19:21:19 +0700

    SLIDE 40

--- php/index.php
+++ php/index.php
@@ -1,27 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
 <?php
-    function dislay_image_list($imagelist)
-    {
-        foreach ($imagelist as $item)
-        {
-            echo "<img src='images/$item'
-            width = 50px height 50px alt = '$item'>";
-        }
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $db = "myDB";
+    $conn = new mysqli($servername, $username,  $password, $db);
+    if ($conn->connect_error){
+        die("Connection failed: " .$conn->connect_error);
     }
-    $items  = array('anh1.png','anh2.png');
-    dislay_image_list($items);
+    echo "Connected successfully <br>"
 ?>
-<h1>function</h1>
-<?php
-    $items  = array('anh3.png','anh4.png');
-    dislay_image_list($items);
-?>
-</body>
-</html>
