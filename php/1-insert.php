fc61aea8245730429494c64abbed7b6a9ea6de08
DucManh2710 <tdmanh2710@gmail.com>
2023-12-06 15:12:27 +0700

    Slide 55

--- php/1-insert.php
+++ php/1-insert.php
@@ -1,6 +1,20 @@
 <?php
     require 'connect.php';
-    $sql = "INSERT INTO flights (Origin, Destination, Duration) VALUES ('Hà Nội', 'Paris', '4000')";
-    $conn -> query($sql);
-    $conn -> close();
+    mysqli_set_charset($conn, 'UTF8');
+    $sql = "SELECT * FROM flights";
+    $reselt = $conn->query($sql);
+    if ($reselt->num_rows > 0)
+    {
+        while($row = $reselt->fetch_assoc())
+        {
+            echo "FlightID: " . $row["id"].
+                 " Origin: " . $row["origin"].
+                 " Destination: " . $row["destination"].
+                 " Duration: " . $row["duration"]. "<br>";
+        }
+    }
+    else {
+        echo "No flights in database";
+    }
+    $conn->close();
 ?>
