52106ff0f2ebe1c0ecb9ea9c51e1e23d1d9f6ad0
DucManh2710 <tdmanh2710@gmail.com>
2023-12-06 15:06:10 +0700

    SLIDE 46 new

--- php/1-insert.php
+++ php/1-insert.php
@@ -1,13 +1,6 @@
 <?php
     require 'connect.php';
     $sql = "INSERT INTO flights (Origin, Destination, Duration) VALUES ('Hà Nội', 'Paris', '4000')";
-    if ($conn->query($sql) === TRUE)
-    {
-        echo "New flight created successfully"; 
-    }
-    else
-    {
-        echo "Error: " . $sql . "<br>" . $conn->error;
-    }
-    $conn->close();
+    $conn -> query($sql);
+    $conn -> close();
 ?>
