3e0f1a96540987f8903bda4e7f3b542ddda40a13
DucManh2710 <tdmanh2710@gmail.com>
2023-12-06 15:29:46 +0700

    SLIDE 60

--- php/1-insert.php
+++ php/1-insert.php
@@ -4,5 +4,11 @@
     $sql = "SELECT * FROM flights";
     $reselt = $conn->query($sql);
     print_r($reselt);
+    $row = $reselt->fetch_assoc();
+    echo "<br>";
+    print_r($row);
+    $row = $reselt->fetch_assoc();
+    echo "<br>";
+    print_r($row);
     $conn->close();
-?>
+    ?>
